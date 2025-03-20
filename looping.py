c8bcd4a4f4dcf7f3f965d8c875b626c39aafa37d
LOUSHIABBAS <87332573+LOUSHIABBAS@users.noreply.github.com>
2025-03-20 15:35:12 -0400

    added looping learning

---
+++ looping.py
@@ -0,0 +1,34 @@
+# Title: Loops learning
+# Author: Ali Abbas
+# Date: 3 20 2025
+
+
+'''
+fruits = ["apple", "banana", "cherry"]
+
+
+def loopOrNo():
+    while True:
+        wannaloop = input("Do you want to loop? ").strip().lower()
+        if wannaloop in ["yes", "no"]:
+            return wannaloop
+        print("Invalid choice. Please enter 'yes' or 'no'.")
+        
+
+
+def fruitsFunc(userchoice):
+    if userchoice == "yes":
+        for fruit in fruits:
+           print(f"I like {fruit}")
+    else: 
+        print(fruits)
+   
+
+def main():
+    wannaloop = loopOrNo()
+    fruitsFunc(wannaloop)
+
+
+if __name__ == "__main__":
+    main()
+'''
